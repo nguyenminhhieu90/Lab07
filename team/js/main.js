94b03e041e43e1f02822b799542679c43088ef94
nguyenminhhieu90 <minhhieukma@gmail.com>
2023-11-04 21:32:25 +0700

    Bai tap 7

---
+++ team/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".navbar-toggle").click(function(){
+        // $("ul#responsive-menu").slideToggle();
+        $("ul#responsive-menu").slideToggle();
+    });
+});
